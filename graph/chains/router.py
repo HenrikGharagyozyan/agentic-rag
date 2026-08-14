b9f52e33162d3aba08e8abe2c764b11b76a37802
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-08-14 19:46:22 +0400

    feat: replace ChatOllama with ChatGoogleGenerativeAI in router for improved query routing

--- graph/chains/router.py
+++ graph/chains/router.py
@@ -2,7 +2,7 @@ from typing import Literal
 
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
-from langchain_ollama import ChatOllama
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 class RouteQuery(BaseModel):
@@ -14,7 +14,10 @@ class RouteQuery(BaseModel):
     )
 
 
-llm = ChatOllama(model="qwen3:1.7b", temperature = 0)
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash",
+    temperature=0,
+)
 structured_llm_router = llm.with_structured_output(RouteQuery)
 
 system = """You are an expert at routing a user question to a vectorstore or web search.
